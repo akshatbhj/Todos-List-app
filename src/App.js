ea2db9f688f63c518a14be529ea04afd62e6eeae
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2023-05-24 16:11:54 +0530

    Creating Todos, Todo and Footer Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Todo from "./components/Todo";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
     <Header/>
+    <Todos/>
+    <Todo/>
+    <Footer/>
     </>
   );
 }
